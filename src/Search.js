313da733e5189f388d8819afe79c8fd76f9aa389
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-21 12:12:39 -0700

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 
 export default function Search(props) {
   let [city, setCity] = useState("");
+  let [display, setDisplay] = useState(false);
+  let [temperature, setTemperature] = useState("");
+  let [description, setDescription] = useState("");
+  let [humidity, setHumidity] = useState("");
+  let [wind, setWind] = useState("");
+  let [icon, setIcon] = useState("");
 
   let form = (
     <form
@@ -29,9 +35,22 @@ export default function Search(props) {
     </button>
   </form>
   );
+  function showTemperature(response) {
+    setDisplay(true);
+    setCity(response.data.name);
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(Math.round(response.data.wind.speed));
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
 
   function handleSearch(event) {
-    event.preventDefault();
+    event.preventDefault();    
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=dc249be89a0015c3980887c32be65599`;
+    axios.get(url).then(showTemperature);
   }
   function updateCity(event) {
     setCity(event.target.value);
