02a85f7e77cfcd9c6d4a29fd42b74820607d0c4a
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-21 11:23:29 -0700

    files update

---
+++ src/Forecast.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div className="Forecast">
+      <div className="row px-1 mx-auto">
+        <div className="col-6 col-sm-4 col-xl-2 py-3">
+          <div className="row justify-content-center days">MON</div>
+          <div className="row justify-content-center">
+            <span className="text-center" role="img" aria-label="emoji">
+              ☀️
+            </span>
+          </div>
+          <div className="row justify-content-center">81˚/ 61˚</div>
+        </div>
+        <div className="col-6 col-sm-4 col-xl-2 py-3">
+          <div className="row justify-content-center days">TUE</div>
+          <div className="row justify-content-center">
+            <span className="text-center" role="img" aria-label="emoji">
+              ☀️
+            </span>
+          </div>
+          <div className="row justify-content-center">81˚/ 61˚</div>
+        </div>
+        <div className="col-6 col-sm-4 col-xl-2 py-3">
+          <div className="row justify-content-center days">WED</div>
+          <div className="row justify-content-center">
+            <span className="text-center" role="img" aria-label="emoji">
+              ☀️
+            </span>
+          </div>
+          <div className="row justify-content-center">81˚/ 61˚</div>
+        </div>
+        <div className="col-6 col-sm-4 col-xl-2 py-3">
+          <div className="row justify-content-center days">THU</div>
+          <div className="row justify-content-center">
+            <span className="text-center" role="img" aria-label="emoji">
+              ☀️
+            </span>
+          </div>
+          <div className="row justify-content-center">81˚/ 61˚</div>
+        </div>
+        <div className="col-6 col-sm-4 col-xl-2 py-3">
+          <div className="row justify-content-center days">FRI</div>
+          <div className="row justify-content-center">
+            <span className="text-center" role="img" aria-label="emoji">
+              ☀️
+            </span>
+          </div>
+          <div className="row justify-content-center">81˚/ 61˚</div>
+        </div>
+        <div className="col-6 col-sm-4 col-xl-2 py-3">
+          <div className="row justify-content-center days">SAT</div>
+          <div className="row justify-content-center">
+            <span className="text-center" role="img" aria-label="emoji">
+              ☀️
+            </span>
+          </div>
+          <div className="row justify-content-center">81˚/ 61˚</div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Search.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export default function Search(props) {
+  let [city, setCity] = useState("");
+
+  function handleSearch(event) {
+    event.preventDefault();
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container-fluid">
+        <form
+          onSubmit={handleSearch}
+          className="d-inline-flex flex-fill flex-wrap flex-sm-nowrap align-items-center p-2 mb-2"
+        >
+          <input
+            onChange={updateCity}
+            className="form-control me-2 my-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            value="Search"
+            id="search"
+            className="btn btn-secondary mx-1 p-1"
+            type="submit"
+          >
+            Search
+          </button>
+          <button className="btn btn-secondary mx-1 p-1" type="submit">
+            Current
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
