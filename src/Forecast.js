2cfedde16b62727ac65506fe10ec16089fa072d9
tiffanymackay <88056784+tiffanymackay@users.noreply.github.com>
2021-09-22 11:01:56 -0700

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -13,41 +13,7 @@ export default function Search() {
           </div>
           <div className="row justify-content-center">81˚/ 61˚</div>
         </div>
-        <div className="col-6 col-sm-4 col-md-2 col-xl-2 py-3">
-          <div className="row justify-content-center days">TUE</div>
-          <div className="row justify-content-center icons">
-          <WeatherIcon code="01d" />
-          </div>
-          <div className="row justify-content-center">81˚/ 61˚</div>
-        </div>
-        <div className="col-6 col-sm-4 col-md-2 col-xl-2 py-3">
-          <div className="row justify-content-center days">WED</div>
-          <div className="row justify-content-center icons">
-          <WeatherIcon code="01d" />
-          </div>
-          <div className="row justify-content-center">81˚/ 61˚</div>
-        </div>
-        <div className="col-6 col-sm-4 col-md-2 col-xl-2 py-3">
-          <div className="row justify-content-center days">THU</div>
-          <div className="row justify-content-center icons">
-          <WeatherIcon code="01d" />
-          </div>
-          <div className="row justify-content-center">81˚/ 61˚</div>
-        </div>
-        <div className="col-6 col-sm-4 col-md-2 col-xl-2 py-3">
-          <div className="row justify-content-center days">FRI</div>
-          <div className="row justify-content-center icons">
-          <WeatherIcon code="01d" />
-          </div>
-          <div className="row justify-content-center">81˚/ 61˚</div>
-        </div>
-        <div className="col-6 col-sm-4 col-md-2 col-xl-2 py-3">
-          <div className="row justify-content-center days">SAT</div>
-          <div className="row justify-content-center icons">
-          <WeatherIcon code="01d" />
-          </div>
-          <div className="row justify-content-center">81˚/ 61˚</div>
-        </div>
+        
       </div>
     </div>
   );
